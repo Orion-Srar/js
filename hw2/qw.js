b0e60ae2bcd0a1f9a38499872691bcbcaf9b66c0
boyko <boykoandriy93@gmail.com>
2022-07-27 11:51:52 +0300

    add hw2,js

---
+++ hw2/qw.js
@@ -0,0 +1,57 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+// let arrys = ["one", "two", "three", "four", "five", 23, 35, 345, true, "reed"];
+// console.log(arrys[0]);
+// console.log(arrys[1]);
+// console.log(arrys[2]);
+// console.log(arrys[3]);
+// console.log(arrys[4]);
+// console.log(arrys[5]);
+// console.log(arrys[6]);
+// console.log(arrys[7]);
+// console.log(arrys[8]);
+// console.log(arrys[9]);
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+//     let book = {
+//         title: "Dracula",
+//         pageCount: 255,
+//         genre:	["Horror", 'Gothic']
+//     }
+//     console.log(book);
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+//     let book = {
+//         title: "Dracula",
+//         pageCount: 255,
+//         genre:	["Horror", 'Gothic'],
+//         authors:[
+//             {name:'Bram Stoken',age:35},
+//         ]
+//     }
+// console.log(book);
+// console.log(book.authors[0].name);
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+
+let arrys = [
+    {name:"Vasia", username: "gdfg", password:12356},
+    {name:"Tolia", username: "hghf", password:25465},
+    {name:"Andrii", username: "bnbn", password:56464},
+    {name:"Ivan", username: "hhjj", password:64542},
+    {name:"Petro", username: "ytyt", password:65322},
+    {name:"Den", username: "rert", password:35263},
+    {name:"Mark", username: "ewew", password:32682},
+    {name:"Lola", username: "gfgt", password:36982},
+    {name:"Sasha", username: "hgfg", password:36823},
+    {name:"Vita", username: "fgrt", password:36593},
+]
+console.log(arrys[0].password);
+console.log(arrys[1].password);
+console.log(arrys[2].password);
+console.log(arrys[3].password);
+console.log(arrys[4].password);
+console.log(arrys[5].password);
+console.log(arrys[6].password);
+console.log(arrys[7].password);
+console.log(arrys[8].password);
+console.log(arrys[9].password);
