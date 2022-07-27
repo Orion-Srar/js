443ac5acebf1515aeac216e7d65646f993a14248
boyko <boykoandriy93@gmail.com>
2022-07-27 11:53:43 +0300

    add hw2,

--- hw2/qw.js
+++ hw2/qw.js
@@ -33,25 +33,25 @@
 
 // - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
 
-let arrys = [
-    {name:"Vasia", username: "gdfg", password:12356},
-    {name:"Tolia", username: "hghf", password:25465},
-    {name:"Andrii", username: "bnbn", password:56464},
-    {name:"Ivan", username: "hhjj", password:64542},
-    {name:"Petro", username: "ytyt", password:65322},
-    {name:"Den", username: "rert", password:35263},
-    {name:"Mark", username: "ewew", password:32682},
-    {name:"Lola", username: "gfgt", password:36982},
-    {name:"Sasha", username: "hgfg", password:36823},
-    {name:"Vita", username: "fgrt", password:36593},
-]
-console.log(arrys[0].password);
-console.log(arrys[1].password);
-console.log(arrys[2].password);
-console.log(arrys[3].password);
-console.log(arrys[4].password);
-console.log(arrys[5].password);
-console.log(arrys[6].password);
-console.log(arrys[7].password);
-console.log(arrys[8].password);
-console.log(arrys[9].password);
+// let arrys = [
+//     {name:"Vasia", username: "gdfg", password:12356},
+//     {name:"Tolia", username: "hghf", password:25465},
+//     {name:"Andrii", username: "bnbn", password:56464},
+//     {name:"Ivan", username: "hhjj", password:64542},
+//     {name:"Petro", username: "ytyt", password:65322},
+//     {name:"Den", username: "rert", password:35263},
+//     {name:"Mark", username: "ewew", password:32682},
+//     {name:"Lola", username: "gfgt", password:36982},
+//     {name:"Sasha", username: "hgfg", password:36823},
+//     {name:"Vita", username: "fgrt", password:36593},
+// ]
+// console.log(arrys[0].password);
+// console.log(arrys[1].password);
+// console.log(arrys[2].password);
+// console.log(arrys[3].password);
+// console.log(arrys[4].password);
+// console.log(arrys[5].password);
+// console.log(arrys[6].password);
+// console.log(arrys[7].password);
+// console.log(arrys[8].password);
+// console.log(arrys[9].password);
